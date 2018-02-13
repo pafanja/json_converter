037ca9332b642bc1bb7eb9a8eba1d43fd3686976
pafanja <pafanja@ukr.net>
2018-02-13 14:49:37 +0200

    update some logic

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-$(function() {
+window.onload = function () {
   var arr = [];
   function toStr(obj,prefix){
     prefix = prefix || '';
     switch (typeof obj){
       case 'object':
-        if (Array.isArray(obj))
-          arr.push(prefix + ':' + JSON.stringify(obj));
+        arr.push(prefix + ':' + JSON.stringify(obj));
         var output = "";
-        for (var k in obj){
-          if (obj.hasOwnProperty(k))
-            output += toStr(obj[k], prefix + '.' + k);
-        }
+          Object.keys(obj).forEach(function (key) {
+              output += toStr(obj[key], prefix + '.' + key);
+          });
         return output;
-      case 'function': return "";
       default:
         arr.push(prefix + ':' + obj);//with values
     }
   }
   function delSymbol (sym, sym2, arr) {
-    for (var j=0; j<arr.length; j++) 
-        if (arr[j].match(sym) || arr[j].match(sym2)) delete arr[j];
+    for (var j=0; j<arr.length; j++){
+        if (arr[j].indexOf(sym) !== -1 || arr[j].indexOf(sym2) !== -1) { delete arr[j] };
+    }
+    //залишаю delete , так як arr.splice(j,1) нічого не видаляє
+      // match змінив на indexOf
 }
-  //Зробив вхідний JSON в один рядок, того шо вибивало "invalid or unexpected token"
+  //Якшо брати xmlhttp то вибиває No 'Access-Control-Allow-Origin', а Гугл нічого не видав по вирішенню для static server
   const inputJson = '{"root":{"design":{"options":[{"val":1},{"val":2},{"val":3}]},"comments":[0,1,2],"data":{"inner":[{"array":[{"x":35}]},{"array":[{"y":15}]}]}}}';
   var parsedJson = JSON.parse(inputJson);
   toStr(parsedJson);
   //delete elements
-  delSymbol(',' , '"', arr);
+  delSymbol(',','"',arr);
   //clear undefined elem
-  arr = arr.filter(function(n){ return n != undefined }); 
+  arr = arr.filter(function(n){ return n != undefined });
   //convert to json
   var outputJson = JSON.stringify(arr);
   console.log(outputJson);
-});
+}
 
